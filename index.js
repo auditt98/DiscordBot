2d1d1f6af7cdf778fbd7f864bc646259e5fd7ad2
auditt98 <vietanh8i1998@gmail.com>
2022-01-12 22:22:51 +0700

    >

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const rest = new REST({ version: '9' }).setToken(token);
 })();
 
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
