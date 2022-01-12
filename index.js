c2ffb1ad01095cf35725253feaee2f5765a138e4
auditt98 <vietanh8i1998@gmail.com>
2022-01-12 22:21:13 +0700

    .

--- index.js
+++ index.js
@@ -40,10 +40,6 @@ const rest = new REST({ version: '9' }).setToken(token);
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
@@ -51,6 +47,12 @@ client.on('interactionCreate', async interaction => {
     await interaction.reply('Pong!');
   }
 });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
 
 client.on('messageCreate', (message) => {
   console.log('message')
@@ -83,38 +85,7 @@ client.on('messageCreate', (message) => {
       })
     }
   }
-})
-client.on('message', (message) => {
-  console.log('message')
-  if(message.content.startsWith(prefix)){
-    console.log('message')
-    let command = message.content.substring(1).toLowerCase();
-    if(command === 'ping'){
-      axios.get('https://mcapi.us/server/status?ip=play.ethereal-mc.net')
-      .then(function (response) {
-        response = response.data
-        if(response.status){
-          if(response.status === 'success'){
-            if(response.online){
-              message.reply({
-                content: `Ethereal MC is online. Current players: ${response.players.now}`
-              })
-            } else {
-              message.reply({
-                content: `Ethereal MC is offline. Error: ${message.error}`
-              })
-            }
-          } else {
-            message.reply({
-              content: `Ethereal MC is offline. Error: ${message.error}`
-            })
-          }
-        }
-      }).catch(error => {
-        console.log(error)
-      })
-    }
-  }
-})
+});
+
 keepAlive();
 client.login(token);
