883ae5d3c9f7c37d2f2f5f2dcf2e7354bdd536e0
auditt98 <vietanh8i1998@gmail.com>
2022-01-12 22:03:50 +0700

    .

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { Routes } = require('discord-api-types/v9');
 const token = process.env['token']
 const CLIENT_ID = process.env['client_id']
 const GUILD_ID = process.env['guild_id']
+const axios = require('axios');
+const prefix = '-'
+const keepAlive = require('./server');
 const commands = [{
   name: 'ping',
   description: 'Replies with Pong!'
@@ -41,4 +44,35 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
+client.on('messageCreate', (message) => {
+  if(message.content.startsWith(prefix)){
+    let command = message.content.substring(1).toLowerCase();
+    if(command === 'ping'){
+      axios.get('https://mcapi.us/server/status?ip=play.ethereal-mc.net')
+      .then(function (response) {
+        response = response.data
+        if(response.status){
+          if(response.status === 'success'){
+            if(response.online){
+              message.reply({
+                content: `Ethereal MC is online. Current players: ${response.players.now}`
+              })
+            } else {
+              message.reply({
+                content: `Ethereal MC is offline. Error: ${message.error}`
+              })
+            }
+          } else {
+            message.reply({
+              content: `Ethereal MC is offline. Error: ${message.error}`
+            })
+          }
+        }
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }
+})
+keepAlive();
 client.login(token);
